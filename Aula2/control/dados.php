fc04fff491549221a21498504401c8103fc3ed19
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2025-03-06 19:26:33 -0300

    aula 2

---
+++ Aula2/control/dados.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Dados{
+        private $nome;
+        private $email;
+
+        public function setNome($nm){
+            $this->nome = $nm;
+        }
+
+        public function setEmail($em){
+            $this->email = $em;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function getEmail(){
+            return $this->email;
+        }
+           
+    }
+
+?>
